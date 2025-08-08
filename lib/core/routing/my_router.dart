8b7b8687dafb2b5e814dd79fa69b4353323983c3
isroilovismoiljon <i.isroilov4515@gmail.com>
2025-08-08 17:40:37 +0500

    Test 123323

--- lib/core/routing/my_router.dart
+++ lib/core/routing/my_router.dart
@@ -1,2 +1,12 @@
-class MyRouter{
+import 'package:exam_project/core/routing/routers.dart';
+import 'package:go_router/go_router.dart';
+
+class MyRouter {
+  GoRouter router = GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+        path: Routers.home,
+      ),
+    ],
+  );
 }
